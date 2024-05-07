b0646e8bd08a08ba15924798c0b3bb47ab17b08f
John Deutscher <john@deutscherfamily.com>
2024-05-06 17:24:27 -0700

    getting a little closer

--- app/page.js
+++ app/page.js
@@ -1,13 +1,12 @@
 import { client } from 'react';
 import FlipBoard from './flipboard.client.js';
-import FlipChar from './flipboard.client.js';
 
 export default function Home() {
     return (
         <div>
             <client>
                 <div>
-                    <FlipBoard text="Hello World"></FlipBoard>
+                    <FlipBoard text="Hello"></FlipBoard>
                 </div>
             </client>
         </div>
